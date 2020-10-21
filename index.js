085c110c6b613cb74388e8780ea79bdd391279c5
Sarah <70615195+smanter82@users.noreply.github.com>
2020-10-20 22:41:18 -0400

    added js file and set up html table for scheduler

---
+++ index.js
@@ -0,0 +1,8 @@
+// let saveBtn = document.querySelector(".saveBtn")
+// let activity = document.querySelector(".activity")
+
+// saveBtn.addEventListener("click", function(event){
+//     event.preventDefault()
+//     let savedStuff = activity.value
+//     console.log(savedStuff)
+// })
